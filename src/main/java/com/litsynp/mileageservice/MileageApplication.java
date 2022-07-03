1abeece9177f9e303987c0c38f1f196a29d9675a
litsynp <nocte55is@gmail.com>
2022-07-03 14:38:20 +0900

    build: init project

---
+++ src/main/java/com/litsynp/mileageservice/MileageApplication.java
@@ -0,0 +1,12 @@
+package com.litsynp.mileageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MileageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MileageApplication.class, args);
+    }
+}
---
+++ src/test/java/com/litsynp/mileageservice/MileageApplicationTests.java
@@ -0,0 +1,12 @@
+package com.litsynp.mileageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MileageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
