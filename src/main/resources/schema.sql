2f0d791dfe117bee3846584516be05e5001230f7
litsynp <nocte55is@gmail.com>
2022-07-09 18:19:32 +0900

    feat: update schema.sql with indices

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,13 @@
-# drop table if exists photo;
-# drop table if exists place;
-# drop table if exists review;
-# drop table if exists user_point;
-# drop table if exists users;
+create table users
+(
+    id       BINARY(16)   not null,
+    email    varchar(255) not null unique,
+    password varchar(255),
+    primary key (id)
+) engine = InnoDB;
+
+alter table users
+    add index users_ak01 (email);
 
 create table photo
 (
@@ -15,6 +20,13 @@ create table photo
     primary key (id)
 ) engine = InnoDB;
 
+alter table photo
+    add constraint photo_fk01
+        foreign key (review_id) references review (id);
+
+alter table photo
+    add index photo_ak01 (review_id);
+
 create table place
 (
     id         BINARY(16) not null,
@@ -23,6 +35,12 @@ create table place
     name       varchar(255),
     primary key (id)
 ) engine = InnoDB;
+
+alter table place
+    add unique place_ak01 (name);
+
+alter table place
+    add index place_ak02 (name);
 
 create table review
 (
@@ -35,6 +53,23 @@ create table review
     primary key (id)
 ) engine = InnoDB;
 
+alter table review
+    add constraint review_fk01
+        foreign key (user_id) references users (id);
+
+alter table review
+    add constraint review_fk02
+        foreign key (place_id) references place (id);
+
+ALTER TABLE review
+    add unique review_ak01 (user_id, place_id);
+
+alter table review
+    add index review_ak02 (user_id);
+
+alter table review
+    add index review_ak03 (place_id);
+
 create table user_point
 (
     id         BINARY(16) not null,
@@ -46,38 +81,15 @@ create table user_point
     primary key (id)
 ) engine = InnoDB;
 
-create table users
-(
-    id       BINARY(16)   not null,
-    email    varchar(255) not null,
-    password varchar(255),
-    primary key (id)
-) engine = InnoDB;
-
-alter table users
-    add constraint user_AK01 unique (email);
-
-alter table photo
-    add constraint FKnx36dfpxbxmxifyiq7yvhiohn
-        foreign key (review_id)
-            references review (id);
-
-alter table review
-    add constraint FKn429agmmvh298piqrnnd4gbfg
-        foreign key (place_id)
-            references place (id);
-
-alter table review
-    add constraint FK6cpw2nlklblpvc7hyt7ko6v3e
-        foreign key (user_id)
-            references users (id);
+alter table user_point
+    add constraint user_point_fk01
+        foreign key (user_id) references users (id);
+alter table user_point
+    add constraint user_point_fk02
+        foreign key (review_id) references review (id);
 
 alter table user_point
-    add constraint FKem95jv3kjy52bx01ros3ct5ti
-        foreign key (review_id)
-            references review (id);
+    add index user_point_ak01 (user_id);
 
 alter table user_point
-    add constraint FKl7fer3vdwhdrg5tj3rhwf130n
-        foreign key (user_id)
-            references users (id);
+    add index user_point_ak02 (review_id)
