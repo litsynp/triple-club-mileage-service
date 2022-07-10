bfd593ceeadb197e5923a0d3dfc1287f6a9c14e4
litsynp <nocte55is@gmail.com>
2022-07-10 22:27:55 +0900

    fix: fix jackson serialization of LocalDateTime

---
+++ src/main/java/com/litsynp/mileageservice/global/config/JacksonConfig.java
@@ -0,0 +1,18 @@
+package com.litsynp.mileageservice.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        return objectMapper;
+    }
+}
--- src/main/java/com/litsynp/mileageservice/global/config/WebConfig.java
+++ src/main/java/com/litsynp/mileageservice/global/config/WebConfig.java
@@ -1,15 +1,40 @@
 package com.litsynp.mileageservice.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig extends WebMvcConfigurationSupport {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
         super.addResourceHandlers(registry);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new PageableHandlerMethodArgumentResolver());
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // Register custom object mapper disabled from WebConfig
+        // https://stackoverflow.com/questions/7854030/configuring-objectmapper-in-spring
+        converters.stream().filter(MappingJackson2HttpMessageConverter.class::isInstance)
+                .forEach(c ->
+                        ((MappingJackson2HttpMessageConverter) c).setObjectMapper(this.objectMapper)
+                );
     }
 }
