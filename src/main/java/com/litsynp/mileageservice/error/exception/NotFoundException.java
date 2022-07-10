bffc85bec5a02ede36c6e233b141f6b1ec1cac02
litsynp <nocte55is@gmail.com>
2022-07-10 18:42:15 +0900

    fix: add space to message prefix for exceptions

--- src/main/java/com/litsynp/mileageservice/error/exception/NotFoundException.java
+++ src/main/java/com/litsynp/mileageservice/error/exception/NotFoundException.java
@@ -6,6 +6,6 @@ import java.util.UUID;
 public class NotFoundException extends BusinessException {
 
     public NotFoundException(String resourceName, UUID id) {
-        super(resourceName + "not found with id = " + id, ErrorCode.ENTITY_NOT_FOUND);
+        super(resourceName + " not found with id = " + id, ErrorCode.ENTITY_NOT_FOUND);
     }
 }
--- src/main/java/com/litsynp/mileageservice/error/exception/NotFoundFieldException.java
+++ src/main/java/com/litsynp/mileageservice/error/exception/NotFoundFieldException.java
@@ -5,7 +5,7 @@ import com.litsynp.mileageservice.error.ErrorCode;
 public class NotFoundFieldException extends BusinessException {
 
     public NotFoundFieldException(String resourceName, String fieldName, String fieldValue) {
-        super(resourceName + "not found with " + fieldName + " = " + fieldValue,
+        super(resourceName + " not found with " + fieldName + " = " + fieldValue,
                 ErrorCode.INNER_ENTITY_NOT_FOUND);
     }
 }
